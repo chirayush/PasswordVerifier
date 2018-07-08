bbd7f0523fd60252648dd03b4f096ec85edb85b9
Charlie Agarwal <chirayush.agarwal@aa.com>
2018-07-08 16:07:58 -0500

    Passed all requirements

--- src/main/java/PasswordVerify.java
+++ src/main/java/PasswordVerify.java
@@ -1,4 +1,64 @@
-public class PasswordVerify {
-    public void verify(String a) {
+import org.apache.commons.lang3.CharUtils;
+
+final class PasswordVerify {
+    boolean verify(final String password) {
+        if (password == null) {
+            throw new IllegalArgumentException("Password Should Not Be Null");
+        }
+        if (!hasLowercase(password)) {
+            throw new IllegalArgumentException("Password Should Have Atleast 1 lowercase letter");
+        }
+        int num = 2;
+        if (hasSufficientLength(password)) num--;
+        if (hasUppercase(password)) num--;
+        if (hasNumeric(password)) num--;
+
+        if (num <= 0) return true;
+
+        if (!hasUppercase(password)) {
+            throw new IllegalArgumentException("Password Should Have Atleast 1 uppercase letter");
+        }
+        if (!hasNumeric(password)) {
+            throw new IllegalArgumentException("Password Should Have Atleast 1 Numeric");
+        }
+        if (!hasSufficientLength(password)) {
+            throw new IllegalArgumentException("Password should Be Atleast 8 characters long");
+        }
+
+        return true;
+    }
+
+    private boolean hasSufficientLength(String password) {
+        return password.length() > 8;
+    }
+
+    private boolean hasNumeric(String password) {
+        char[] passChars = password.toCharArray();
+        for (char passChar : passChars) {
+            if (CharUtils.isAsciiNumeric(passChar)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean hasLowercase(String password) {
+        char[] passChars = password.toCharArray();
+        for (char passChar : passChars) {
+            if (CharUtils.isAsciiAlphaLower(passChar)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean hasUppercase(String password) {
+        char[] passChars = password.toCharArray();
+        for (char passChar : passChars) {
+            if (CharUtils.isAsciiAlphaUpper(passChar)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
--- src/test/java/PasswordVerifyTest.java
+++ src/test/java/PasswordVerifyTest.java
@@ -2,17 +2,63 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
+import static junit.framework.TestCase.assertTrue;
+
 public class PasswordVerifyTest {
 
     @Rule
     public ExpectedException expectedException = ExpectedException.none();
+    private PasswordVerify passwordVerify = new PasswordVerify();
+
+//    @Test
+//    public void verifyShouldReturnExceptionIfPasswordLesserThan8Chars() {
+//        expectedException.expect(IllegalArgumentException.class);
+//        expectedException.expectMessage("Password should Be Atleast 8 characters long");
+//        passwordVerify.verify("aA12");
+//    }
 
     @Test
-    public void verifyShouldReturnExceptionIfPasswordLesserThan8Chars() {
-        expectedException.expect(IllegalArgumentException.class);
-        expectedException.expectMessage("Password should Be Atleast 8 characters long");
-        PasswordVerify passwordVerify = new PasswordVerify();
-        passwordVerify.verify("a");
+    public void verifyShouldNotThrowExceptionIfPasswordGreaterThan8Chars() {
+        assertTrue(passwordVerify.verify("AbCd1R390"));
+    }
 
+    @Test
+    public void verifyShouldThrowExceptionIfPasswordIsNull() {
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage("Password Should Not Be Null");
+        passwordVerify.verify(null);
+    }
+
+    @Test
+    public void verifyShouldThrowExceptionIfDoesNotHaveAtleast1UpperCaseLetter() {
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage("Password Should Have Atleast 1 uppercase letter");
+        passwordVerify.verify("abcdersdfd");
+    }
+
+    @Test
+    public void verifyShouldThrowExceptionIfDoesNotHaveAtleast1LowerCaseLetter() {
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage("Password Should Have Atleast 1 lowercase letter");
+        passwordVerify.verify("ABCSDKFLER");
+    }
+
+    @Test
+    public void verifyShouldThrowExceptionIfDoesNotHaveAtleast1Number() {
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage("Password Should Have Atleast 1 Numeric");
+        passwordVerify.verify("ABCacf");
+    }
+
+    @Test
+    public void verifyShouldReturnTrueIfPasswordUpperLowerAndNumeric() {
+        assertTrue(passwordVerify.verify("Ab1R390"));
+    }
+
+    @Test
+    public void verifyShouldThrowExceptionIfNoLowerCase() {
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage("Password Should Have Atleast 1 lowercase letter");
+        passwordVerify.verify("ABCSDKFL234ER");
     }
 }
